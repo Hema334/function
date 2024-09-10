5b6b1c00d9f99209f4b2cd40b081c19731c512b3
Hema334 <112267828+Hema334@users.noreply.github.com>
2024-09-10 16:30:01 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int add()
+{
+int a=20;
+int b=30;
+int c=a+b;
+return c;
+}
+void main()
+{
+    int ans=add();
+    printf("%d",ans);
+    
+}
